63c351d1cd09b239c312bea582272d1eb7cd1cd9
vtimiryanov <76KsYW&rqXa3eWQC>
2021-04-04 19:21:02 +0300

    testing

--- uploder.php
+++ uploder.php
@@ -1,6 +1,17 @@
 <?php
-if (move_uploaded_file($_FILES['file']['tmp_name'], 'files/'.$_FILES['file']['name'])) {
+if ($_FILES['files']['size'] > 100000){
+    echo 'Првевышен допустимый размер файла 100мб';
+    exit();
+}
+$newfile = 'files/'.$_FILES['file']['name'];
+if (move_uploaded_file($_FILES['file']['tmp_name'], $newfile)) {
+    if(file_exists($newfile)){
+        echo 'файл с таким названием уже существует!<br> <a href='/'>назад</a>';
+        exit();
+    }
+
     echo "Файл был успешно загружен.<br> <a href='/'>назад</a>";
+
 } else {
     echo "Произошла ошибка при загрузки файла ";
 }
